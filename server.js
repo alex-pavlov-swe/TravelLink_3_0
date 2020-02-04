703b55ebc9f86e0a8c42463b1f954e2485f54b37
PavlovAS <pavlov.cmc@gmail.com>
2020-02-03 22:50:41 -0800

    prepared for deployment

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use('/api/reviews', require('./routes/api/reviews'));
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // set static folder
-  app.use(expree.static('client/build'));
+  app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
